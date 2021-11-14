133661f9403bdb5e681546c11512a4907cdd7fe7
dpavloff <Dan43575@gmail.com>
2021-11-14 12:43:06 +0500

    1-3

--- src/helpers/validators.js
+++ src/helpers/validators.js
@@ -1,6 +1,6 @@
 /**
  * @file Домашка по FP ч. 1
- * 
+ *
  * Основная задача — написать самому, или найти в FP библиотеках функции anyPass/allPass
  * Эти функции/их аналоги есть и в ramda и в lodash
  *
@@ -14,34 +14,51 @@
  */
 
 // 1. Красная звезда, зеленый квадрат, все остальные белые.
-export const validateFieldN1 = ({star, square, triangle, circle}) => {
-    if (triangle !== 'white' || circle !== 'white') {
-        return false;
-    }
+export const validateFieldN1 = ({ star, square, triangle, circle }) => {
+  if (triangle !== "white" || circle !== "white") {
+    return false;
+  }
 
-    return star === 'red' && square === 'green';
+  return star === "red" && square === "green";
 };
 
 // 2. Как минимум две фигуры зеленые.
-export const validateFieldN2 = () => false;
+export const validateFieldN2 = ({ star, square, triangle, circle }) => {
+  return (
+    [star, square, triangle, circle].filter((x) => x === "green").length > 1
+  );
+};
 
 // 3. Количество красных фигур равно кол-ву синих.
-export const validateFieldN3 = () => false;
+export const validateFieldN3 = ({ star, square, triangle, circle }) => {
+  return (
+    [star, square, triangle, circle].filter((x) => x === "red").length ===
+    [star, square, triangle, circle].filter((x) => x === "blue").length
+  );
+};
 
 // 4. Синий круг, красная звезда, оранжевый квадрат
-export const validateFieldN4 = () => false;
+export const validateFieldN4 = ({ star, square, triangle, circle }) => {
+  return circle === "blue" && star === "red" && square === "orange";
+};
 
 // 5. Три фигуры одного любого цвета кроме белого (четыре фигуры одного цвета – это тоже true).
-export const validateFieldN5 = () => false;
+export const validateFieldN5 = ({ star, square, triangle, circle }) => {};
 
 // 6. Две зеленые фигуры (одна из них треугольник), еще одна любая красная.
 export const validateFieldN6 = () => false;
 
 // 7. Все фигуры оранжевые.
-export const validateFieldN7 = () => false;
+export const validateFieldN7 = ({ star, square, triangle, circle }) => {
+  return (
+    [star, square, triangle, circle].filter((x) => x === "orange").length === 4
+  );
+};
 
 // 8. Не красная и не белая звезда.
-export const validateFieldN8 = () => false;
+export const validateFieldN8 = ({ star, square, triangle, circle }) => {
+  return star !== "red" || star !== "white";
+};
 
 // 9. Все фигуры зеленые.
 export const validateFieldN9 = () => false;
